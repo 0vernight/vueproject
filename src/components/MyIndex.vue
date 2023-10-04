0287ac5a91cba1aa3231933f397f417f590801ab
0vernight <2323659522@qq.com>
2023-10-04 18:00:59 +0800

    On branch main  Initial commit  Changes to be committed: 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .prettierrc.json 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/HelloWorld.vue 	new file:   src/components/MyIndex.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/main.js 	new file:   vite.config.js
     Changes not staged for commit:
    	modified:   src/components/MyIndex.vue

---
+++ src/components/MyIndex.vue
@@ -0,0 +1,51 @@
+<script setup>
+// import MyIndex from './MyIndex.vue'
+// import DocumentationIcon from './icons/IconDocumentation.vue'
+// import ToolingIcon from './icons/IconTooling.vue'
+// import EcosystemIcon from './icons/IconEcosystem.vue'
+// import CommunityIcon from './icons/IconCommunity.vue'
+// import SupportIcon from './icons/IconSupport.vue'
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      <p id="idpm" :class="pm" name="p">my index may be you will doing great and best i gees!</p>
+      <!-- You’ve successfully created a project with
+            <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+            <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. -->
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,81 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+//import axios from 'https://unpkg.com/axios/dist/axios.min.js'
+
+// this.Vue.prototype.axios = axios
+
+createApp(App).mount('#app')
+
+window.onload =()=> {
+    
+    let el = document.getElementsByClassName("green");
+    el.value = "tiaozhuan";
+    console.log('myconsole=' + el)
+    let pc = document.getElementsByClassName(".pm");
+    let pi = document.getElementById("idpm");
+    console.log("class =" + pc  );
+    console.log("id ="+  pi );
+    //pc.style ="red";
+    pi.style.color = 'white';
+    pi.style.backgroundColor = 'green';
+    // let p = document.createElement("p");
+    // p.textContent="哈哈哈哈";
+    // p.insertBefore(el);
+
+
+}
+// const promise =new Promise((resolve, reject) => {
+
+//     if (promise.then.resolve) {
+//         resolve();
+//     } else {
+//         reject();
+
+//     }
+// })
+// console(promise.then.arguments)
+// function a(){
+//       return new Promise(function(res,rej){
+//         $.ajax({
+//             url: '',
+//             method: 'POST',
+//             datatype: 'json',
+//             success: data => {
+//                 res(data);
+//             },
+//             error: err => {
+//                 rej(err);
+//             }
+//         })
+//       });
+//     }
+// function b(data){
+//       console.log(data,"data");
+//       return new Promise(function(res,rej){
+//         $.ajax({
+//             url:"b接口",
+//             type: "POST",
+//             async:true,
+//             data:JSON.stringify(data),
+//             dataType:"json",
+//             success:function(data){
+//               console.log(data,"b");
+//               res();
+//             }
+//           })
+//       });
+// }
+// function c(data) {
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//         axios.get();
+//     })
+// }
+// $("#btn").click(function(){
+//       a().then(function (data){
+//         b(data);
+//       }).then(function(){
+//       })
+// })
